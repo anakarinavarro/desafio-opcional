3ce1cd0b3d88c435d7233c0f8de16bac64b08867
ana navarro <anakarina.stratus@gmail.com>
2022-04-29 12:37:12 -0400

    desafio opcional 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+<h1>Desafío Opcional Vue Cli</h1>
+
+<table class="table table-striped">
+  <thead>
+    <tr>
+      
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellido</th>
+      <th scope="col">Run</th>
+      <th scope="col">Fecha de nacimiento</th>
+      <th scope="col">Edad</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(usuario, $index) of usuarios" :key="$index">
+      <td>{{usuario.nombre}}</td>
+      <td>{{usuario.apellido}}</td>
+      <td>{{usuario.run}}</td>
+      <td>{{usuario.nacimiento}}</td>
+      <td>{{usuario.edad}}</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: ()=>({
+    usuarios: [
+      {nombre:"Jose", apellido:"Perez", run:"123456-2", nacimiento:"12-04-67", edad:"52"},
+      {nombre:"Maria", apellido:"Rodriguez", run:"654321-1", nacimiento:"30-11-90", edad:"29"},
+      {nombre:"Manuel", apellido:"Patiño", run:"14223456-6", nacimiento:"02-12-59", edad:"60"}
+    ]
+  })
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
